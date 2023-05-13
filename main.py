4fc9f8a28d4d4aa83e057a075981d64f9ddf76bc
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-05-13 22:52:52 +0200

    adding main while loop in main

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 from procedures import*
 from constants_ import*
 
+
+
 def main():
-    inp_ = input("Type here: ")
 
-    for i in inp_:
-        if not (i in characters.DIGITS or i in characters.TOKENS):
-            print("INVALID CHARACTERS")
-            return
+    while True:
+        print("Running. Press q and hit ENTER to quit")
+        inp_ = input("Type here: ")
 
-    procedure = Procedure()
-    made_list = procedure.to_list(inp_)
+        if 'q' in inp_:
+            break
 
-    while len(made_list) > 1:
-        token_list = procedure.get_tokens(made_list)
-        prioritised_token = procedure.get_prioritised_token(token_list)
-        made_list = procedure.pack(prioritised_token[0], made_list)
-    
-        #print(made_list)
-    
-    result = None
+        for i in inp_:
+            if not (i in characters.DIGITS or i in characters.TOKENS):
+                print("INVALID CHARACTERS")
+                return
 
-    while made_list[0].reference.type != "number":
-        procedure.resolve_tree(made_list[0])
+        procedure = Procedure()
+        made_list = procedure.to_list(inp_)
 
-    print(made_list[0].reference.number)
+        while len(made_list) > 1:
+            token_list = procedure.get_tokens(made_list)
+            prioritised_token = procedure.get_prioritised_token(token_list)
+            made_list = procedure.pack(prioritised_token[0], made_list)
+        
+            #print(made_list)
+
+        while made_list[0].reference.type != "number":
+            procedure.resolve_tree(made_list[0])
+
+        print(made_list[0].reference.number)
     
 
 main()
